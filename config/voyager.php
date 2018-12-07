56180f05837655b8e6035527e3b032356c27facc
ristotido <koffiaristide14@yahoo.fr>
2018-12-07 19:55:28 +0000

    Update voyager.php

--- config/voyager.php
+++ config/voyager.php
@@ -124,13 +124,13 @@ return [
         /*
          * Select default language
          */
-        'default' => 'en',
+        'default' => 'fr',
 
         /*
          * Select languages that are supported.
          */
         'locales' => [
-            'en',
+            'fr',
             //'pt',
         ],
     ],
